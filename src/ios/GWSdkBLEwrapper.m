1806d117ad9b46b4e3bdf9a40f3b13c59b575cc9
陈东东 <kevinchen@infapp.com>
2015-11-06 17:55:19 +0800

    修改ios sdk

--- src/ios/GWSdkBLEwrapper.m
+++ src/ios/GWSdkBLEwrapper.m
@@ -10,7 +10,7 @@
 #import <GizDataAccess/GizDataAccess.h>
 #import <Cordova/CDV.h>
 
-@interface GWSdkBLEwrapper: CDVPlugin<GizDataAccessSourceDelegate> {
+@interface GWSdkBLEwrapper: CDVPlugin<GizDataAccessSourceDelegate, GizDataAccessLoginDelegate> {
     // Member variables go here.
     NSString * _appId;
     GizDataAccessSource* gdaSource ;
@@ -22,46 +22,55 @@
 @property (strong,nonatomic) CDVInvokedUrlCommand * commandHolder;
 
 @end
+
 @implementation GWSdkBLEwrapper
 
-
-
 -(void)pluginInitialize:(CDVInvokedUrlCommand *) command{
+    if (_appId==nil) {
+        _appId=@"8110dc34b3dc49b386bcbc692473bd28";
+       GizDataAccess *da = [GizDataAccess startWithAppID:_appId];
+    }
+    //Init SDK
+    
+    
     gdaSource = [[GizDataAccessSource alloc] initWithDelegate:self];
-    //gdaLogin = [[GizDataAccessLogin alloc] initWithDelegate:self];
-
+    gdaLogin = [[GizDataAccessLogin alloc] initWithDelegate:self];
+    
 }
 
 -(void)login:(CDVInvokedUrlCommand *)command
 {
-  self.commandHolder = command;
-
+    self.commandHolder = command;
+    [self pluginInitialize:command];
     [gdaLogin loginAnonymous];
 }
+
 - (void)gizDataAccessDidLogin:(GizDataAccessLogin *)login uid:(NSString *)uid token:(NSString *)token result:(GizDataAccessErrorCode)result message:(NSString *)message {
     if(result == kGizDataAccessErrorNone) {
         // 登录成功
         // ……
         NSLog(@"登录成功");
-         NSDictionary *d = [NSDictionary dictionaryWithObjectsAndKeys:
-                                   @"登陆成功", @"state",
-                                   nil];
-          CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:d];
-                [self.commandDelegate sendPluginResult:pluginResult callbackId:self.commandHolder.callbackId];
+        NSDictionary *d = [NSDictionary dictionaryWithObjectsAndKeys:
+                           @"登陆成功", @"state",
+                           nil];
+        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:d];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:self.commandHolder.callbackId];
     }
 }
+
 /**
  支持批量数据上传。最后一个参数data为数组类型，可以指定每一组数据的产生时间和内容，格式为标准的JSON。上传结果，通过数据上传的委托返回。
  
  data示例： @{@"ts": [timestamp], @"attrs": @{ [dynamic_keys]: [dynamic_values], ... } }
-
+ 
  */
 -(void)writeData:(CDVInvokedUrlCommand *)command{
     NSString *token=command.arguments[0];
     NSString *productKey=command.arguments[1];
     NSString *deviceSn=command.arguments[2];
     NSArray *data=command.arguments[3];
-    
+    self.commandHolder = command;
+    [self pluginInitialize:command];
     [gdaSource saveData:token productKey:productKey deviceSN:deviceSn data:data];
 }
 
@@ -80,8 +89,8 @@
     NSString *deviceSn=command.arguments[2];
     int16_t startTime=command.arguments[3];
     int16_t endTime=command.arguments[4];
-
-     [gdaSource loadData:token productKey:productKey deviceSN:deviceSn startTime:startTime endTime:endTime limit:20 skip:0];
+    
+    [gdaSource loadData:token productKey:productKey deviceSN:deviceSn startTime:startTime endTime:endTime limit:20 skip:0];
 }
 /*实现获取数据的委托方法
  
